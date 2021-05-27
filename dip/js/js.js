ad816013fa34d957ea3fbb711ba60cbbf372d6bc
VladykaTMY <82215415+VladykaTMY@users.noreply.github.com>
2021-05-27 14:16:22 +0300

    Промежуточный результат

--- dip/js/js.js
+++ dip/js/js.js
@@ -1,8 +1,8 @@
 let nums = document.querySelectorAll('.btn_key_num');
 let displayInfo = document.getElementById('displayInfo');
 let oper = document.querySelectorAll('.btn_key_oper')
-
-//вводит данные 
+let tbl= document.getElementById('tbl')
+//вводим данные и добавляем слушетеля  на нажатие  по кнопокам
 for(let y=0; y<nums.length;y++){
     nums[y].addEventListener("click",()=>{
     displayInfo.innerText += +nums[y].value;
@@ -11,18 +11,32 @@ for(let y=0; y<nums.length;y++){
 
 for(let i=0;i<oper.length;i++){
     oper[i].addEventListener("click", ()=>{
-        displayInfo.innerHTML += oper[i].value;
+        displayInfo.innerText += oper[i].value;
     });
 }
-
-function clearValue() {
+// чистое поле ввода
+function cleanValue() {
     return displayInfo.innerText = " ";
     
 }
-
+//вычисляем
 function calcValue() {
-   return  displayInfo.innerHTML=eval(displayInfo.innerText);
+   return  displayInfo.innerText=eval(displayInfo.innerText);
     
 }
+//выводим в таблицу последнюю операцию
+function getLastValue() {
+    tbl.children[1].rows[0].innerText=displayInfo.innerText;
+}
 
+//вычисляем логарифмы
+function calcLog(){
+    let result= Math.log(Number(displayInfo.innerText));
+    displayInfo.innerText = result;
+}
 
+//вычисляем квадратный корень
+function calcSqrt(){
+    let result= Math.sqrt(Number(displayInfo.innerText));
+    displayInfo.innerText = result;
+}
